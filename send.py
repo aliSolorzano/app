76acdcf29926b9d963bdb61ec8b4a8e7c043f10c
Ali Canaima <alistrange140@gmail.com>
2020-02-10 17:14:38 -0400

    Ajustando

--- send.py
+++ send.py
@@ -15,10 +15,10 @@ def mensaje():
     message = elcontenido.get()
 
     # setup the parameters of the message
-    password = "19072001"
+    password = suContra.get()
     msg['From'] = micorreo.get()
     msg['To'] = sucorreo.get()
-    msg['Subject'] = "Hackeado"
+    msg['Subject'] = elasunto.get()
 
     # add in the message body
     msg.attach(MIMEText(message, 'plain'))
@@ -53,24 +53,30 @@ micorreo = tk.StringVar()
 deCorreo = Entry(root,textvariable=micorreo,width=50).grid(column=2,row=1,)
 
 
+lblContra = Label(root,text="Contraseña:",bg="white",pady=10,font=disenoText).grid(column=1,row=2,sticky=E)
+suContra = tk.StringVar()
+Contra = Entry(root,textvariable=suContra,width=50).grid(column=2,row=2)
+
+
+
 #Aqui esta el texto y la entrada del correo de para quien es
-lblPara = Label(root,text="Para:",bg="white",pady=10,font=disenoText).grid(column=1,row=2,sticky=E)
+lblPara = Label(root,text="Para:",bg="white",pady=10,font=disenoText).grid(column=1,row=3,sticky=E)
 sucorreo = tk.StringVar()
-deCorreo = Entry(root,textvariable=sucorreo,width=50).grid(column=2,row=2)
+deCorreo = Entry(root,textvariable=sucorreo,width=50).grid(column=2,row=3)
 
 
 #aqui va el asunto
-asunto = Label(root,bg="white",text="Asunto",pady=10,font=disenoText).grid(column=1,row=3,sticky=E)
+asunto = Label(root,bg="white",text="Asunto",pady=10,font=disenoText).grid(column=1,row=4,sticky=E)
 elasunto = tk.StringVar()
-Asunto = Entry(root,textvariable=elasunto,width=50).grid(column=2,row=3)
+Asunto = Entry(root,textvariable=elasunto,width=50).grid(column=2,row=4)
 
 
-contenido = Label(root,bg="white",text="Contenido",pady=20,font=disenoText).grid(column=1,row=4,sticky=E)
+contenido = Label(root,bg="white",text="Contenido",pady=20,font=disenoText).grid(column=1,row=5,sticky=E)
 elcontenido = tk.StringVar()
-msj = Entry(root,textvariable=elcontenido,width=50).grid(column=2,row=4)
+msj = Entry(root,textvariable=elcontenido,width=50).grid(column=2,row=5)
 
 
 btn = Button(root,text="Enviar",command=mensaje)
-btn.grid(column=2,row=5)
+btn.grid(column=2,row=6)
 
 root.mainloop()
